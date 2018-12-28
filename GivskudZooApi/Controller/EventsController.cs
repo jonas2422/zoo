46601e3aa82bbff7b4f4c0761c826b0c45410905
jonas2422 <31859200+jonas2422@users.noreply.github.com>
2018-12-28 12:27:54 +0100

    API
    News api without doctypes

---
+++ GivskudZooApi/Controller/EventsController.cs
@@ -0,0 +1,21 @@
+﻿using GivskudZooApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Umbraco.Core.Services;
+using Umbraco.Web.WebApi;
+
+namespace GivskudZooApi.Controller
+{
+    class EventsController : UmbracoApiController
+    {
+        [HttpGet]
+        public List<NewsItems> All() {
+            IContentService cs = Services.ContentService;
+            var home = cs.GetById();
+        }
+    }
+}
---
+++ GivskudZooApi/Models/NewsItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GivskudZooApi.Models
+{
+    public class NewsItems
+    {
+        public int id { get; set; }
+        public string EventItem { get; set; }
+        public string EventDesc { get; set; }
+    }
+}
